c8cc7b1eeb460bc011b3a476246799c197a035e9
ga-syu0106 <s.01vanishment06@gmail.com>
2020-12-26 12:36:02 +0900

    編集機能の実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -9,8 +9,8 @@ class PrototypesController < ApplicationController
   end
 
   def create
-    @prototype = Prototype.new(prototype_params)
-    if @prototype.save
+    prototype = Prototype.new(prototype_params)
+    if prototype.save
       redirect_to root_path
     else 
       render :new
@@ -22,11 +22,18 @@ class PrototypesController < ApplicationController
   end
 
   def edit
+    @prototype = Prototype.find(params[:id])
   end
 
   def update
+    @prototype = Prototype.find(params[:id])
+    if @prototype.update(prototype_params)
+      redirect_to prototype_path
+    else
+      render :edit
+    end
   end
-  
+
   private
   def prototype_params
     params.require(:prototype).permit(:title , :catch_copy , :concept , :image).merge(user_id: current_user.id)
