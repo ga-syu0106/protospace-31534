2b2b264b714e653e57e43ef564759671eb7bef64
ga-syu0106 <s.01vanishment06@gmail.com>
2021-04-28 21:52:21 +0900

    Change from HTTPS to SSH

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -9,7 +9,6 @@ class PrototypesController < ApplicationController
   def new
     @prototype = Prototype.new
   end
-
   def create
     @prototype = Prototype.new(prototype_params)
     if @prototype.save
